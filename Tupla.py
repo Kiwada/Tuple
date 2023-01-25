703b1d2cb8ecb4a80b2f4f9aca5bb07e673567a5
Kaio Fontenele <82303943+Kiwada@users.noreply.github.com>
2023-01-25 12:36:56 -0300

    Add files via upload

---
+++ Tupla.py
@@ -0,0 +1,10 @@
+Lanche = ('Hambúrger','Suco','Pizza','Pudim')
+for cont in range(0, len(Lanche)):
+    print(f'Eu vou comer {Lanche[cont]}') #New formatting method 
+print('Comi pra CARAI')
+
+#Segundo Metodo Sem o Len
+
+#for comida in Lanche:
+#   print(f'Eu vou comer {comida}')
+#print('Comi pra caramba')
